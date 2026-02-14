71bef3bbfb4839233a41dd2185fcaefc141b156c
henriqueenuness <henriquenunesborg@gmail.com>
2026-02-14 09:33:45 -0300

    ip

--- incept/incept/settings.py
+++ incept/incept/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'batata_frita'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000', '74.220.49.0/24', '74.220.57.0/24']
 
 
 # Application definition
