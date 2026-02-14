39cedd566d31c4b23342ba50e634de590717e119
henriqueenuness <henriquenunesborg@gmail.com>
2026-02-14 09:24:32 -0300

    commit de conserto

--- incept/incept/settings.py
+++ incept/incept/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,19 +78,18 @@ WSGI_APPLICATION = 'incept.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'supabase': { 
+    'default': dj_database_url.config( 
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}' )
+}
+
+"""    'supabase': { 
         'ENGINE': 'django.db.backends.postgresql', 
         'NAME': 'postgres', 
         'USER': 'postgres', 
         'PASSWORD': 'incept2026*', 
         'HOST': 'db.sloqpnvutcreuxstuldh.supabase.co', 
         'PORT': '5432', 
-        }
-}
+        }"""
 
 
 # Password validation
@@ -130,3 +131,9 @@ STATIC_URL = '/static/'
 """STATICFILES_DIRS = [BASE_DIR / "static"]"""
 
 LOGOUT_REDIRECT_URL = "login_pg"
+
+
+
+
+
+
