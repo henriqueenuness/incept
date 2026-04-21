a3e59a4fa2e882c760b7de8d89578e662ec3d45c
henriqueenuness <henriquenunesborg@gmail.com>
2026-04-21 10:30:48 -0300

    remove firebase e adiciona waitress nos requirements

--- incept/settings.py
+++ incept/settings.py
@@ -14,12 +14,7 @@ import dj_database_url
 from dotenv import load_dotenv
 from decouple import config
 from pathlib import Path
-import firebase_admin
-from firebase_admin import credentials
 
-
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
