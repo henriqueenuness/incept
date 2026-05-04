c41e169ceefecb38dec522f607a5f55bb6e1ad1c
OliveiraEGL <joaopedrooliveiraaguiar090@gmail.com>
2026-05-04 10:16:09 -0300

    começando função de reportar post

--- feed/models.py
+++ feed/models.py
@@ -82,3 +82,15 @@ class Saved(models.Model):
         on_delete=models.CASCADE,
         null = True
     )
+
+class Reports(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        null = True
+    )
+    reason = models.TextField(null=False)
